bc5a19708d86f7f17e57b33c5db87e233a7770e0
samaparo <mirthquakes@gmail.com>
2016-02-09 22:01:37 -0500

    proof of concept for localstorage

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 (function () {
-  var colors = ['blue', 'green', 'red'];
-  var content = document.getElementsByClassName('content')[0];
-  var i = 0;
-  window.setInterval(function () {
-    content.classList.remove('blue', 'green', 'red');
-    content.classList.add(colors[i]);
-    i = i < 2 ? i + 1 : 0;
-  }, 1000);
+  function writeToStorage() {
+    if(navigator.onLine) {
+      localStorage.setItem('lastUpdate', (new Date()).toString());
+    }
+    document.getElementsByClassName('content')[0].innerHTML = "Last Update: " + localStorage.getItem('lastUpdate');
+  }
+  window.setInterval(writeToStorage, 1000);
+  writeToStorage();
 }());
